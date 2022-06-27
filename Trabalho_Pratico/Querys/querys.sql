f38fd89fd0f2db5cecb0d8b4237dfefc3d050894
Vinny021 <viniciusbarbosa021@gmail.com>
2022-06-27 17:05:49 -0300

    View, Function and Roles

--- Trabalho_Pratico/Querys/querys.sql
+++ Trabalho_Pratico/Querys/querys.sql
@@ -22,7 +22,40 @@ CREATE TABLE pokemon_families (
 );
 
 CREATE TABLE regions (
-    name char(20) primary key,
+    id int primary key,
+    name char(20),
     id_first_pokemon int not null,
     id_last_pokemon int not null
 );
+
+CREATE FUNCTION pokemon_evolutions(id int)
+  RETURNS TABLE (pokemon_id text, pokemon_name text)
+AS
+$body$
+  SELECT dex_num, name  FROM pokemons
+  WHERE evolution_familly_id = $1 
+$body$
+language sql;
+
+CREATE VIEW pokemons_infos AS 
+    SELECT 
+        p.name as "Nome", 
+        p.type1 as "Tipo Principal",
+        p.type2 as "Tipo Secundário",
+        r.name as "Região" FROM 
+    pokemons p INNER JOIN regions r ON  
+        dex_num >= id_first_pokemon AND dex_num <= id_last_pokemon;
+
+CREATE ROLE "User";
+GRANT SELECT ON pokemons_infos TO "User";
+GRANT EXECUTE ON FUNCTION pokemon_evolutions TO "User";
+
+CREATE ROLE "Gerenciador"
+GRANT 
+    SELECT, UPDATE, DELETE, INSERT 
+ON 
+    pokemon_families, pokemons, regions, sprites 
+TO "Gerenciador";
+
+CREATE ROLE "Admin";
+GRANT ALL ON ALL TABLES IN SCHEMA "public" TO "Admin";
